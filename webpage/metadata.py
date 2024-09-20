247878cffe71d989705fe4bb909ae5d31613663a
csae8092 <peter.andorfer@oeaw.ac.at>
2024-09-20 14:57:31 +0200

    some layout things

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -2,12 +2,11 @@
 # (by default) in the base.html and index.html
 
 PROJECT_METADATA = {
-    'title': 'Django Base Project',
+    'title': 'Manuscripta ',
     'author': 'Peter Andorfer',
-    'subtitle': 'A django project to bootstrap further web-app developments',
-    'description': 'This django projects includes some generic applications\
-    (webpage, vocabs, entities, bib)',
-    'github': 'https://github.com/acdh-oeaw/djangobaseproject',
+    'subtitle': 'Mittelalterliche Handschriften in Österreich ',
+    'description': 'A prototype Django implementation of manuscripta.at',
+    'github': 'https://github.com/acdh-oeaw/manuscripta-django',
     'purpose_de': 'der Schaffung einer einheitlichen\
     und generischen Grundlage für Web Applikationen.',
     'purpose_en': 'to bootstrap web development.',
@@ -15,9 +14,6 @@ PROJECT_METADATA = {
     'matomo_id': 'provide some',
     'matomo_url': '//matomo.acdh.oeaw.ac.at/',
     'imprint': '/imprint',
-    'social_media': [
-        ('fab fa-twitter fa-2x', 'https://twitter.com/ACDH_OeAW'),
-        ('fab fa-youtube fa-2x', 'https://www.youtube.com/channel/UCgaEMaMbPkULYRI5u6gvG-w'),
-    ],
+    'social_media': [],
     'app_type': 'database',  # database|website|service|tool|digital-edition
 }
